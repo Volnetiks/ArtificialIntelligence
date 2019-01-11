adf28bfedfc6dab6eeeb7d5f40315875ee96f884
Volnetiks <thomas.bechu@free.fr>
2019-01-11 16:12:04 +0100

    Initial Commit: Create main classe and .gitignore file

--- src/com/volnetiks/ai/ArtificialIntelligence.java
+++ src/com/volnetiks/ai/ArtificialIntelligence.java
@@ -3,6 +3,8 @@ package com.volnetiks.ai;
 /* Date: 11/01/2019 For Artificial Intelligence By Volnetiks */
 public class ArtificialIntelligence {
 
+    public static void main(String[] args) {
 
+    }
 
 }
